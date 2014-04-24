4b303771193c7d9856f81853ec19bc24f4bf8c76
jerryshew <jerryshew@gmail.com>
2014-04-24 18:25:16 +0800

    refactoring

--- module/require.js
+++ module/require.js
@@ -1,17 +1,24 @@
 window.onload = function(){
 	var prevImg = document.getElementById("previous_image"),
 			nextImg = document.getElementById("next_image");
-			var marquee = new	Marquee("image_scroll_display", "original_display", "clone_display");
+	var marquee = new	Marquee("image_scroll_display", "original_display", "clone_display");
+	var lastImg = document.getElementById("clone_display"),
+			contentEnd = document.getElementById("image_scroll_display").style.offsetLeft;
 			prevImg.onclick = function(){
-				marquee.scrollLeft(1000);
+				marquee.rollingLeft(2000);
+				return false;
 			};
+			// nextImg.onclick = function() {
+			// 	marquee.rollingRight(2000);
+			// };
 				
 };
 
 var Animation = {
-	easeIn: function(width, time, hSpeed){
-		
-		 
+	easeIn: function(width){
+		var speed;
+		// speed = 800 * width - width*width;
+		return speed;
 	},
 	liner: function(){
 
@@ -23,33 +30,37 @@ var Marquee = function(container, original, clone){
 			original = document.getElementById(original),
 			clone =  document.getElementById(clone);
 			clone.innerHTML = original.innerHTML;
-	// var rolling = function(){
- //    if(container.scrollLeft == clone.offsetLeft){
- //      container.scrollLeft = 0;
- //    }else{
- //    	container.scrollLeft++;
- //    }
- //  }
- // 	var timer = setInterval(rolling, 20);
- // 	container.onmouseover = function(){clearInterval(timer)};
- // 	container.onmouseout = function(){timer = setInterval(rolling, 20)};
+	var goLeft = function(){
+    if(container.scrollLeft == clone.offsetLeft){
+      container.scrollLeft = 0;
+    }else{
+    	container.scrollLeft+=10;
+    }
+  }
+ 	// var timer = setInterval(rolling, 20);
+ 	// container.onmouseover = function(){clearInterval(timer)};
+ 	// container.onmouseout = function(){timer = setInterval(rolling, 20)};
 
   return{
-  	scrollLeft: function(width){
-  		var width = width||1000;
+  	rollingLeft: function(){
+  		var timer = setInterval(goLeft, 20);
+  	},
+  	rollingRight: function(width){
+  		var width = width||600;
   		if(container.scrollLeft == clone.offsetLeft){
   			container.scrollLeft = 0;
   		}else{
   			setInterval(function(){
-  				if (container.scrollLeft < width) {
-  					container.scrollLeft += 100;
-  				};
-  			},20);
-  			// container.scrollLeft += width;
-  		}
-  	},
-  	scrollRight: function(){
 
+  				// if (container.scrollLeft > width) {
+  					// for(var i=0; i < width; i++){
+							// container.scrollLeft += Animation.easeIn(i);
+							// console.log(container.scrollLeft);
+  					// }
+  					container.scrollLeft --;
+  				// }
+  			},100);
+  		}
   	}
   }
 }
