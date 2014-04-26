d8e1ca640d1b23ce5c6441b795c671ecc5c067b3
jerryshew <jerryshew@gmail.com>
2014-04-26 15:34:30 +0800

    navigation bar

---
+++ dialog/main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+
+}
+
+var template = {
+	
+};
---
+++ navibar/mian.js
@@ -0,0 +1,6 @@
+window.onscroll = function(){
+	var scroll_height = document.documentElement.scrollTop || document.body.scrollTop;
+	// if (scroll_height > 10) {
+		console.log("scroll_height");
+	// };
+}
