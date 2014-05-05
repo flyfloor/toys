23dd1a4b6526faca2c4d71c037f125df0b5a7c8a
jerryshew <jerryshew@gmail.com>
2014-05-05 17:58:32 +0800

    e.... nothing

--- parallax scrolling/main.js
+++ parallax scrolling/main.js
@@ -1,39 +1,44 @@
 var Ned = {
 	appear: function(){
-		$("#ned").stop().find("img").fadeIn(1000);
+		$("#ned").stop().find("img").fadeIn(500);
 	},
 	disappear: function(){
-		$("#ned").stop().find("img").fadeOut(1000);
+		$("#ned").stop().find("img").fadeOut(500);
 	}
 }
 
 var Robb = {
 	appear: function(){
-		$("#robb").find(".info").stop().animate({left: "700px", opacity:1}, 800);
+		$("#robb").find(".info").stop().animate({left: "700px", opacity:1}, 200);
 	},
 	disappear: function(){
-		$("#robb").find(".info").stop().animate({left: "1200px",opacity:0}, 800);
+		$("#robb").find(".info").stop().animate({left: "1200px",opacity:0}, 200);
 	}
 };
+
+var intervalTimer = null;
+
+function mouseWheel(event){
+	event.preventDefault();
+	clearTimeout(intervalTimer);
+
+	intervalTimer = setTimeout(function(){
+		if (event.wheelDelta < 0) {
+			$("body, html").stop().animate({scrollTop: '+=800'}, "slow");
+		}else{
+			$("body, html").stop().animate({scrollTop: '-=800'}, "slow");
+		}
+
+	}, 500);
+};  
 
 $(document).ready(function(){
 	var fullHeight = $(document).scrollHeight;
 
-	window.onmousewheel = function mouseWheel(event){
-		event.preventDefault();
-		console.log();
-		setTimeout(function(){
-			if (event.wheelDelta < 0) {
-				$("body, html").stop().animate({scrollTop: '+=800'}, "slow");
-			}else{
-				$("body, html").stop().animate({scrollTop: '-=800'}, "slow");
-			}
-		},400);
-		return false;
+	window.onmousewheel = function(event){
+		mouseWheel(event);
 	};
 
-
-	
 
 	var $badge = $("#badge");
 
@@ -43,12 +48,12 @@ $(document).ready(function(){
 		//animtations
 		if (topHeight >= 300 & topHeight <= 900) {
 			Ned.appear();
-			$badge.stop().animate({top: "70px", left: "120px"}, 1000);
+			$badge.stop().animate({top: "70px", left: "120px"}, 200);
 		}else{
 			Ned.disappear();
 			if(topHeight > 900 & topHeight <= 1800){
 				Robb.appear();
-				$badge.stop().animate({top: "250px", left: "1100px"}, 1000);
+				$badge.stop().animate({top: "250px", left: "1100px"}, 200);
 			}else if (topHeight > 1800) {
 				Robb.disappear();
 			}	
