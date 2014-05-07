db3d003ee87cd6b317281f4a4b4f95f55c71a54f
jerryshew <jerryshew@gmail.com>
2014-05-07 18:07:11 +0800

    auto match input

--- automatch/main.js
+++ automatch/main.js
@@ -1,23 +1,32 @@
-window.myCallback = function(data){
-	console.log(data);
-}
-
 $(document).ready(function(){
 
-	$("button").on("click", function(){
-		$.ajax({
-			url: "http://127.0.0.1:4000/fetch",
-			method: "GET",
-			dataType: "jsonp",
-			crossDomain: true,
-			success: function(data){
-				console.log(data);
-			},
-			error: function(xhr, status, error){
-				console.log(error);
-			}
-		});
+	$("#automatch").on("keyup", function(event){
+		var keyCode = event.keyCode,
+				inputStr = $(this).val(),
+			 	$list = $("ul#hints");				
+
+		if (inputStr) {			
+			$.ajax({
+				url: "http://127.0.0.1:4000/fetch?q="+ inputStr,
+				method: "GET",
+				dataType: "jsonp",
+				crossDomain: true,
+				success: function(data){
+					$list.find("li").remove();
+					for(var i = 0; i < data.length; i++ ){
+						var $item = $("<li></li>").addClass("hit-item");
+						$item.text(data[i].name);
+						$item.appendTo($list);
+					}
+				},
+				error: function(xhr, status, error){
+					console.log(error);
+				}
+			});
+		}
 
 	});
+
+
 });
 
--- automatch/server/requestHandlers.js
+++ automatch/server/requestHandlers.js
@@ -1,31 +1,24 @@
+var querystring = require("querystring");
+var url = require("url");
+
 var data = ["Anna","Brittany","Cinderella","Diana","Eva","Fiona","Gunda","Hege","Inga","Johanna","Kitty","Linda","Nina","Ophelia","Petunia","Amanda","Raquel","Cindy","Doris","Eve","Evita","Sunniva","Tove","Unni","Violet","Liza","Elizabeth","Ellen","Wenche","Vicky"];
 
 function fetchData(request, response){
+  var resData = [],
+  		reqQuery = querystring.parse(url.parse(request.url).query),
+  		reqStr = reqQuery.q.toLowerCase();
 
-	var resData = [
-		{name: data[0]},
-		{name: data[1]},
-		{name: data[2]},
-		{name: data[3]},
-		{name: data[4]},
-		{name: data[5]},
-		{name: data[6]},
-		{name: data[7]},
-		{name: data[8]},
-		{name: data[9]},
-		{name: data[10]},
-		{name: data[11]},
-		{name: data[12]},
-		{name: data[13]},
-		{name: data[14]}
-	];
-
-	// console.log(resData);
+	if (reqStr) {
+		for (var i in data) {
+			if (data[i].toString().toLowerCase().match(reqStr) !== null) {
+				resData.push({name:data[i]});
+			};
+		};
+	};
 
 	response.writeHead(200, {"Content-Type": "application/json"}); 
+  response.end(reqQuery.callback +"(" + JSON.stringify(resData) + ")");
 
-  // response.write(resData);
-  response.end(request.callback + JSON.stringify(resData));
 }
 
 exports.fetch = fetchData;
