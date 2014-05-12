5f3e55732fc789920a8eb7436bbd12fcee30e0e0
jerryshew <jerryshew@gmail.com>
2014-05-12 15:12:16 +0800

    nothing

--- dialog/main.js
+++ dialog/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 									'<p>This document defines a mechanism to enable client-side cross-origin requests. Specifications that enable an API to make cross-origin requests to resources can use the algorithms defined by this specification. If such an API is used on http://example.org resources, a resource on http://hello-world.example can opt in using the mechanism described by this specification (e.g., specifying Access-Control-Allow-Origin: http://example.org as response header), which would allow that resource to be fetched cross-origin from http://example.org.'+
 									'</p>'+
 								'</section>'),
-			blur:false,
+			blur:true,
 			yesFunc: function(){
 				alert("yes button handle");
 			},
